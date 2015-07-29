a7827de2707bc2392a5d6bd049fb75456db48be7
Leonardo Riviere <leonardoriviere@MacBook-Pro-de-Leonardo.local>
2015-07-29 11:38:38 -0300

    Fully functional Complaint Form.

--- src/main/java/org/jboss/errai/demo/client/local/ComplaintForm.java
+++ src/main/java/org/jboss/errai/demo/client/local/ComplaintForm.java
@@ -2,6 +2,7 @@ package org.jboss.errai.demo.client.local;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import org.jboss.errai.common.client.api.Caller;
@@ -14,21 +15,16 @@ import org.jboss.errai.ui.nav.client.local.Page;
 import org.jboss.errai.ui.shared.api.annotations.Bound;
 import org.jboss.errai.ui.shared.api.annotations.DataField;
 import org.jboss.errai.ui.shared.api.annotations.EventHandler;
-import org.jboss.errai.ui.shared.api.annotations.Model;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
-import com.googlecode.gwtphonegap.client.camera.Camera;
-import com.googlecode.gwtphonegap.client.camera.PictureCallback;
-import com.googlecode.gwtphonegap.client.camera.PictureOptions;
 
 /**
  * This is the companion Java class of the complaint form as specified by
@@ -53,27 +49,21 @@ public class ComplaintForm extends Composite {
      * userComplaint.name will automatically be kept in sync with the data-field
      * "name")
      */
-    @Inject
-    @Model
     private UserComplaint userComplaint;
 
     @Inject
-    @Bound
     @DataField
     private TextBox name;
 
     @Inject
-    @Bound
     @DataField
     private TextBox email;
 
     @Inject
-    @Bound
     @DataField
     private TextArea text;
 
     @Inject
-    @Bound
     @DataField
     private ValueImage image;
 
@@ -85,22 +75,13 @@ public class ComplaintForm extends Composite {
     @DataField
     private Button submit;
 
-
     @Inject
     private Caller<TestService> testService;
 
-    private int i = 1;
-
-    /**
-     * Errai's Cordova integration module makes native device hardware injectable
-     * into your code.
-     */
-    @Inject
-    private Camera camera;
-
-    @Inject
-    @DataField
-    private Button takePicture;
+    @PostConstruct
+    public void init() {
+        clear();
+    }
 
     /**
      * This method is registered as an event handler for click events on the
@@ -111,34 +92,67 @@ public class ComplaintForm extends Composite {
 
     @EventHandler("submit")
     private void onSubmit(ClickEvent e) {
+        boolean isNew = userComplaint.getId() == null;
+        buildUserComplaintFromWidgets();
+        if (isNew) {
+            save();
+            clear();
+        } else {
+            update();
+            clear();
+        }
+    }
+
+    private void buildUserComplaintFromWidgets() {
+        userComplaint.setName(name.getText());
+        userComplaint.setText(text.getText());
+        userComplaint.setEmail(email.getText());
+    }
+
+    private void buildWidgetsFromUserComplaint(UserComplaint complaint) {
+        name.setText(complaint.getName());
+        email.setText(complaint.getEmail());
+        text.setText(complaint.getText());
+    }
+
+    private void save() {
         testService.call(new RemoteCallback<Void>() {
             @Override
             public void callback(Void aVoid) {
-                tableGenerator();
+                buildTable();
             }
         }).save(userComplaint);
     }
 
-    private void tableGenerator() {
+    private void update() {
+        testService.call(new RemoteCallback<Void>() {
+            @Override
+            public void callback(Void aVoid) {
+                buildTable();
+            }
+        }).update(userComplaint.getId(), userComplaint);
+    }
+
+    private void clear() {
+        userComplaint = new UserComplaint();
+        name.setText("");
+        email.setText("");
+        text.setText("");
+    }
+
+    private void buildTable() {
         testService.call(new RemoteCallback<List<UserComplaint>>() {
             @Override
             public void callback(List<UserComplaint> userComplaints) {
-                table.setWidget(0,0, new Label("Id"));
-                table.setWidget(0,1, new Label("Name"));
-                table.setWidget(0,2, new Label("Email"));
-                table.setWidget(0,3, new Label("Complaint"));
+                setTableHeader();
                 int j = 0;
                 for (final UserComplaint complaint : userComplaints) {
                     Button edit = new Button("Edit");
                     edit.addClickHandler(new ClickHandler() {
                         @Override
                         public void onClick(ClickEvent clickEvent) {
-                            complaint.setName(name.getText());
-                            complaint.setEmail(email.getText());
-                            complaint.setText(text.getText());
-                            testService.call().update(complaint.getId(),complaint);
-                            table.clear();
-                            tableGenerator();
+                            userComplaint = complaint;
+                            buildWidgetsFromUserComplaint(complaint);
                         }
                     });
                     Button delete = new Button("Delete");
@@ -147,51 +161,29 @@ public class ComplaintForm extends Composite {
                         public void onClick(ClickEvent clickEvent) {
                             testService.call().delete(complaint.getId());
                             table.clear();
-                            tableGenerator();
+                            buildTable();
                         }
                     });
-                    table.setWidget(j + 1, 0, new Label(complaint.getId().toString()));
-                    table.setWidget(j+1,1, new Label(complaint.getName()));
-                    table.setWidget(j+1,2, new Label(complaint.getEmail()));
-                    table.setWidget(j+1,3, new Label(complaint.getText()));
-                    table.setWidget(j+1,4, edit);
-                    table.setWidget(j+1,5, delete);
+                    buildRows(j, complaint, edit, delete);
                     j++;
                 }
             }
         }).getTableInfo();
     }
 
+    private void buildRows(int j, UserComplaint complaint, Button edit, Button delete) {
+        table.setWidget(j + 1, 0, new Label(complaint.getId().toString()));
+        table.setWidget(j+1,1, new Label(complaint.getName()));
+        table.setWidget(j+1,2, new Label(complaint.getEmail()));
+        table.setWidget(j+1,3, new Label(complaint.getText()));
+        table.setWidget(j+1,4, edit);
+        table.setWidget(j+1,5, delete);
+    }
 
-    /**
-     * This method is registered as an event handler for click events on the
-     * takePicture button of the complaint form. When running on devices that have
-     * camera support, it will cause the camera app to open and allow you to take
-     * a picture.
-     *
-     * @param e the click event.
-     */
-
-    @EventHandler("takePicture")
-    private void onTakePictureClick(ClickEvent e) {
-        PictureOptions options = new PictureOptions(25);
-        options.setDestinationType(PictureOptions.DESTINATION_TYPE_DATA_URL);
-        options.setSourceType(PictureOptions.PICTURE_SOURCE_TYPE_CAMERA);
-
-        camera.getPicture(options, new PictureCallback() {
-
-            @Override
-            public void onSuccess(String data) {
-                // On success, we will store the image as a data URL
-                // (https://en.wikipedia.org/wiki/Data_URI_scheme) in our JPA entity.
-                image.setVisible(true);
-                image.setValue("data:image/jpeg;base64," + data, true);
-            }
-
-            @Override
-            public void onFailure(String error) {
-                Window.alert("Could not take picture: " + error);
-            }
-        });
+    private void setTableHeader() {
+        table.setWidget(0,0, new Label("Id"));
+        table.setWidget(0,1, new Label("Name"));
+        table.setWidget(0,2, new Label("Email"));
+        table.setWidget(0,3, new Label("Complaint"));
     }
 }
